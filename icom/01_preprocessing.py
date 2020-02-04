469836bf32eebc0bbb976510ab06fd76eb3f2e5a
Morteza Ansarinia <ansarinia@me.com>
2020-02-04 19:59:17 +0100

    mark lures, n_targets, n_lures, targets, etc in PCL2016 dataset

--- icom/01_preprocessing.py
+++ icom/01_preprocessing.py
@@ -1,2 +1,42 @@
 #%%
-print(12) 
+import numpy as np
+import pandas as pd
+
+from IPython.display import display
+
+
+# load data
+data = pd.read_csv('data/cl2016_nb.csv',index_col=0)
+
+data['N'] = np.where(data.condition=='2-back',2,3)
+data['correct'] = np.where(
+  (data.stimulus_type == data.choice) & (data.stimulus_type != 'burn-in'),
+  True,
+  False
+  )
+
+grouped  = data.groupby(['participant','block','N'])
+
+# group by participant and block
+data['n_targets'] = grouped.stimulus_type.transform(lambda s: s.value_counts()['target']
+)
+
+# find lures
+
+def find_lures(x):
+  N = x.N.values[0]
+  L1 = x.stimulus.shift(N+1)
+  L2 = x.stimulus.shift(N-1)
+  non_targets = (x.stimulus_type != 'target')
+  x['lures'] = (L1.eq(x.stimulus) | L2.eq(x.stimulus)) & non_targets
+  return x[['lures']]
+
+data['lure'] = grouped.apply(find_lures)
+
+data['n_lures'] =  grouped.lure.transform(
+  lambda s: s.count() - s.value_counts()[False]
+)
+
+def recent_targets(x, window=4):
+  N = x.N.values[0]
+  
